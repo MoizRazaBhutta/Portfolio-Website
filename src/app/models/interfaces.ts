296b9b6b9b659557c81eb5cfc159f654f35e88d2
MoizRazaBhutta <moiz_bhutta@hotmail.com>
2024-10-13 19:27:27 -0400

    Interface for models

---
+++ src/app/models/interfaces.ts
@@ -0,0 +1,31 @@
+export interface PersonalInfo{
+    name:string,
+    age:number,
+    email:string,
+    phone:string,
+    address:string
+}
+
+export interface SocialLink{
+    linkedin:string,
+    github:string,
+    facebook:string,
+    instagram:string
+}
+
+export interface Skill{
+    name:string,
+    level:number
+}
+
+export interface Experience{
+    title:string,
+    company:string,
+    duration:string
+}
+
+export interface Education{
+    degree:string,
+    school:string,
+    year:string
+}
